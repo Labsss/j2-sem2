fb235fb20d6551964bf60648bf8d0912ec7f83df
Labsss <alexandre.labonne@gmail.com>
2019-04-09 13:59:11 +0200

    initialisation du repo

---
+++ lib/multiples.rb
@@ -0,0 +1,13 @@
+def is_multiple_of_3_or_5?(current_number)
+=begin     if current_number % 3 == 0
+        return true
+    elsif current_number % 5 == 0
+        return true
+    else return false
+    end 
+=end
+end
+
+def sum_of_3_or_5_multiples(final_number)
+    
+end
